473fe28ddf106cbecf73c60300e3c4baaa841bcd
Josie <josiecollinsrc@gmail.com>
2021-03-26 16:39:48 -0500

    added get routes to category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,22 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  }).then(categories => res.json(categories))
+  .catch(err => res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product],
+  }).then(categories => res.json(categories))
+  .catch(err => res.status(500).json(err))
 });
 
 router.post('/', (req, res) => {
